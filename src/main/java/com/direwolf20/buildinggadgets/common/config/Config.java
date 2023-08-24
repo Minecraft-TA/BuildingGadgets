b4ad10936d2600383405913192ecfc4833ba7bd7
Reinig, Jan <Jan.Reinig@Schenck.net>
2023-08-24 11:34:38 +0200

    Refactor config class to use Forge Configuration API.

--- src/main/java/com/direwolf20/buildinggadgets/common/config/Config.java
+++ src/main/java/com/direwolf20/buildinggadgets/common/config/Config.java
@@ -2,11 +2,13 @@ package com.direwolf20.buildinggadgets.common.config;
 
 import com.direwolf20.buildinggadgets.common.BuildingGadgets;
 import net.minecraft.init.Blocks;
-import net.minecraftforge.common.config.Config.*;
+import net.minecraftforge.common.config.ConfigCategory;
+import net.minecraftforge.common.config.Configuration;
 
 //hardcode this name, so that we use a different config file than before we used Annotations
-@net.minecraftforge.common.config.Config(modid = BuildingGadgets.MODID, name = "Building Gadgets", category = Config.CATEGORY_ROOT)
 public class Config {
+
+    private static final Configuration config = new Configuration(); // TODO: correctly set path
 
     static final String CATEGORY_ROOT = "general";
 
@@ -40,59 +42,24 @@ public class Config {
 
     private static final String LANG_KEY_GADGETS_DURABILITY_COMMENT = "The Gadget's Durability (0 means no durability is used) (Ignored if powered by FE)";
 
-    @RangeDouble(min = 1 , max = 48)
-    @Name("Max Build Distance")
-    @Comment("Defines how far away you can build")
-    @LangKey(LANG_KEY_ROOT + ".rayTraceRange")
-    public static double rayTraceRange = 32;
+    public static double rayTraceRange = config.get(CATEGORY_ROOT, "Max Build Distance", 32, "Defines how far away you can build", 1, 48).setLanguageKey(LANG_KEY_ROOT + ".rayTraceRange").getDouble();
 
-    @RequiresWorldRestart
-    @Name("Powered by Forge Energy")
-    @Comment("Set to true for Forge Energy Support, set to False for vanilla Item Damage")
-    @LangKey(LANG_KEY_ROOT + ".poweredByFE")
-    public static boolean poweredByFE = true;
+    public static boolean poweredByFE = config.get(CATEGORY_ROOT, "Powered by Forge Energy", true, "Set to true for Forge Energy Support, set to False for vanilla Item Damage").setLanguageKey(LANG_KEY_ROOT + ".poweredByFE").setLanguageKey(LANG_KEY_ROOT + ".poweredByFE").setRequiresWorldRestart(true).getBoolean();
 
-    @RequiresMcRestart
-    @RequiresWorldRestart
-    @Name("Enable Construction Paste")
-    @Comment("Set to false to disable the recipe for construction paste.")
-    @LangKey(LANG_KEY_ROOT + ".paste.enabled")
-    public static boolean enablePaste = true;
+    public static boolean enablePaste = config.get(CATEGORY_ROOT, "Enable Construction Paste", true, "Set to false to disable the recipe for construction paste.").setLanguageKey(LANG_KEY_ROOT + ".paste.enabled").setRequiresMcRestart(true).setRequiresWorldRestart(true).getBoolean();
 
-    @RangeInt(min = 0)
-    @Name("Construction Paste Drop Count - Min")
-    @Comment("The minimum number of construction paste items dropped by a dense construction block.")
-    @LangKey(LANG_KEY_ROOT + ".paste.dropped.min")
-    public static int pasteDroppedMin = 1;
+    public static int pasteDroppedMin = config.get(CATEGORY_ROOT, "Construction Paste Drop Count - Min", 1, "The minimum number of construction paste items dropped by a dense construction block.", 0, Integer.MAX_VALUE).setLanguageKey(LANG_KEY_ROOT + ".paste.dropped.min").getInt();
 
-    @RangeInt(min = 0)
-    @Name("Construction Paste Drop Count - Max")
-    @Comment("The maximum number of construction paste items dropped by a dense construction block.")
-    @LangKey(LANG_KEY_ROOT + ".paste.dropped.max")
-    public static int pasteDroppedMax = 3;
+    public static int pasteDroppedMax = config.get(CATEGORY_ROOT, "Construction Paste Drop Count - Max", 3, "The maximum number of construction paste items dropped by a dense construction block.", 0, Integer.MAX_VALUE).setLanguageKey(LANG_KEY_ROOT + ".paste.dropped.max").getInt();
 
-    @RequiresMcRestart
-    @RequiresWorldRestart
-    @Name("Enable Destruction Gadget")
-    @Comment("Set to false to disable the Destruction Gadget.")
-    @LangKey(LANG_KEY_ROOT + ".enableDestructionGadget")
-    public static boolean enableDestructionGadget = true;
+    public static boolean enableDestructionGadget = config.get(CATEGORY_ROOT, "Enable Destruction Gadget", true, "Set to false to disable the Destruction Gadget.").setLanguageKey(LANG_KEY_ROOT + ".enableDestructionGadget").setRequiresMcRestart(true).setRequiresWorldRestart(true).getBoolean();
 
-    @Name("Default to absolute Coord-Mode")
-    @Comment({"Determines if the Copy/Paste GUI's coordinate mode starts in 'Absolute' mode by default.",
-              "Set to true for Absolute, set to False for Relative."})
-    @LangKey(LANG_KEY_ROOT + ".absoluteCoordDefault")
-    public static boolean absoluteCoordDefault = false;
+    public static boolean absoluteCoordDefault = config.get(CATEGORY_ROOT, "Default to absolute Coord-Mode", false, "Determines if the Copy/Paste GUI's coordinate mode starts in 'Absolute' mode by default.\nSet to true for Absolute, set to False for Relative.").setLanguageKey(LANG_KEY_ROOT + ".absoluteCoordDefault").getBoolean();
 
-    @Name("Allow absolute Coord-Mode")
-    @Comment("Disable absolute coords-mode for the Copy-Paste gadget")
-    public static boolean allowAbsoluteCoords = true;
+    public static boolean allowAbsoluteCoords = config.get(CATEGORY_ROOT, "Allow absolute Coord-Mode", true, "Disable absolute coords-mode for the Copy-Paste gadget").setLanguageKey(LANG_KEY_ROOT + ".allowAbsoluteCoords").getBoolean();
 
-    @Name("Allow non-Air-Block-Overwrite")
-    @Comment({"Whether the Building / CopyPaste Gadget can overwrite blocks like water, lava, grass, etc (like a player can).",
-              "False will only allow it to overwrite air blocks."})
-    @LangKey(LANG_KEY_ROOT + ".canOverwriteBlocks")
-    public static boolean canOverwriteBlocks = true;
+    public static boolean canOverwriteBlocks = config.get(CATEGORY_ROOT, "Allow non-Air-Block-Overwrite", true, "Whether the Building / CopyPaste Gadget can overwrite blocks like water, lava, grass, etc (like a player can).\nFalse will only allow it to overwrite air blocks.").setLanguageKey(LANG_KEY_ROOT + ".canOverwriteBlocks").getBoolean();
+
 
     @Name("Blacklist Settings")
     @Comment("Configure your Blacklist-Settings here")
@@ -101,12 +68,7 @@ public class Config {
 
     public static final class CategoryBlacklist {
         //In 1.13 this should be converted to a tag (or at least made compatible with)
-        @Name("Blacklisted Blocks")
-        @Comment({"All Blocks added to this will be treated similar to TileEntities. Not at all.",
-                  "Notice that you can use Regular Expressions as defined by Java Patterns to express more complex name combinations.",
-                  "Use for example \"awfulmod:.*\" to blacklist all awfulmod Blocks."})
-        @LangKey(LANG_KEY_BLACKLIST + " + blockBlacklist")
-        public String[] blockBlacklist = {"minecraft:.*_door.*", PatternList.getName(Blocks.PISTON_HEAD), "astralsorcery:blockflarelight"};
+        public String[] blockBlacklist = config.get(CATEGORY_BLACKLIST, "Blacklisted Blocks", new String[]{"minecraft:.*_door.*", PatternList.getName(Blocks.piston_head), "astralsorcery:blockflarelight"}, "All Blocks added to this will be treated similar to TileEntities. Not at all.\nNotice that you can use Regular Expressions as defined by Java Patterns to express more complex name combinations.\nUse for example \"awfulmod:.*\" to blacklist all awfulmod Blocks.").setLanguageKey(LANG_KEY_BLACKLIST + " + blockBlacklist").getStringList();
     }
 
     @Name("Gadgets")
@@ -125,22 +87,15 @@ public class Config {
         private CategoryGadgets() {
         }
 
-        @RangeInt(min = 1, max = 25)
-        @Name("Maximum allowed Range")
-        @Comment("The max range of the Gadgets")
-        @LangKey(LANG_KEY_GADGETS + ".maxRange")
-        public int maxRange = 15;
+        public int maxRange = config.get(CATEGORY_GADGETS, "Maximum allowed Range", 15, "The max range of the Gadgets", 1, 25).setLanguageKey(LANG_KEY_GADGETS + ".maxRange").getInt();
 
-        @RangeInt(min = 0)
-        @Name("Maximum Energy")
-        @Comment("The max energy of Building, Exchanging & Copy-Paste Gadget")
-        @LangKey(LANG_KEY_GADGETS + ".maxEnergy")
-        public int maxEnergy = 500000;
+        public int maxEnergy = config.get(CATEGORY_GADGETS, "Maximum Energy", 500000, "The max energy of Building, Exchanging & Copy-Paste Gadget", 0, Integer.MAX_VALUE).setLanguageKey(LANG_KEY_GADGETS + ".maxEnergy").getInt();
 
         @Name("Building Gadget")
         @Comment("Energy Cost & Durability of the Building Gadget")
         @LangKey(LANG_KEY_GADGET_BUILDING)
         public CategoryGadgetBuilding subCategoryGadgetBuilding = new CategoryGadgetBuilding();
+
 
         @Name("Exchanging Gadget")
         @Comment("Energy Cost & Durability of the Exchanging Gadget")
@@ -158,112 +113,53 @@ public class Config {
         public CategoryGadgetCopyPaste subCategoryGadgetCopyPaste = new CategoryGadgetCopyPaste();
 
         public static final class CategoryGadgetBuilding {
-            private CategoryGadgetBuilding(){ }
+            private CategoryGadgetBuilding() {
+            }
 
-            @RangeInt(min = 0, max = 100000)
-            @Name("Energy Cost")
-            @Comment(LANG_KEY_GADGETS_ENERGY_COMMENT)
-            @LangKey(LANG_KEY_GADGETS_ENERGY)
-            public int energyCostBuilder = 50;
- 
-            @RangeInt(min = 0, max = 2000)
-            @Name("Damage Cost")
-            @Comment(LANG_KEY_GADGETS_DAMAGE_COMMENT)
-            @LangKey(LANG_KEY_GADGETS_DAMAGE)
-            public int damageCostBuilder = 1;
+            public int energyCostBuilder = config.get(CATEGORY_GADGET_BUILDING, "Energy Cost", 50, LANG_KEY_GADGETS_ENERGY_COMMENT, 0, 100000).setLanguageKey(LANG_KEY_GADGETS_ENERGY).getInt();
 
-            @RequiresWorldRestart
-            @RangeInt(min = 0, max = 100000)
-            @Name("Durability")
-            @Comment(LANG_KEY_GADGETS_DURABILITY_COMMENT)
-            @LangKey(LANG_KEY_GADGETS_DURABILITY)
-            public int durabilityBuilder = 10000;
+            public int damageCostBuilder = config.get(CATEGORY_GADGET_BUILDING, "Damage Cost", 1, LANG_KEY_GADGETS_DAMAGE_COMMENT, 0, 2000).setLanguageKey(LANG_KEY_GADGETS_DAMAGE).getInt();
+
+            public int durabilityBuilder = config.get(CATEGORY_GADGET_BUILDING, "Durability", 10000, LANG_KEY_GADGETS_DURABILITY_COMMENT, 0, 100000).setLanguageKey(LANG_KEY_GADGETS_DURABILITY).setRequiresWorldRestart(true).getInt();
         }
 
         public static final class CategoryGadgetExchanger {
-            private CategoryGadgetExchanger(){ }
+            private CategoryGadgetExchanger() {
+            }
 
-            @RangeInt(min = 0, max = 100000)
-            @Name("Energy Cost")
-            @Comment(LANG_KEY_GADGETS_ENERGY_COMMENT)
-            @LangKey(LANG_KEY_GADGETS_ENERGY)
-            public int energyCostExchanger = 100;
+            public int energyCostExchanger = config.get(CATEGORY_GADGET_EXCHANGER, "Energy Cost", 100, LANG_KEY_GADGETS_ENERGY_COMMENT, 0, 100000).setLanguageKey(LANG_KEY_GADGETS_ENERGY).getInt();
 
-            @RangeInt(min = 0, max = 2000)
-            @Name("Damage Cost")
-            @Comment(LANG_KEY_GADGETS_DAMAGE_COMMENT)
-            @LangKey(LANG_KEY_GADGETS_DAMAGE)
-            public int damageCostExchanger = 2;
+            public int damageCostExchanger = config.get(CATEGORY_GADGET_EXCHANGER, "Damage Cost", 2, LANG_KEY_GADGETS_DAMAGE_COMMENT, 0, 2000).setLanguageKey(LANG_KEY_GADGETS_DAMAGE).getInt();
 
-            @RequiresWorldRestart
-            @RangeInt(min = 0, max = 100000)
-            @Name("Durability")
-            @Comment(LANG_KEY_GADGETS_DURABILITY_COMMENT)
-            @LangKey(LANG_KEY_GADGETS_DURABILITY)
-            public int durabilityExchanger = 10000;
+            public int durabilityExchanger = config.get(CATEGORY_GADGET_EXCHANGER, "Durability", 10000, LANG_KEY_GADGETS_DURABILITY_COMMENT, 0, 100000).setLanguageKey(LANG_KEY_GADGETS_DURABILITY).setRequiresWorldRestart(true).getInt();
         }
 
         public static final class CategoryGadgetDestruction {
-            private CategoryGadgetDestruction () {}
-            @RangeInt(min = 0)
-            @Name("Maximum Energy")
-            @Comment("The max energy of the Destruction Gadget")
-            @LangKey(LANG_KEY_GADGET_DESTRUCTION + ".maxEnergy")
-            public int energyMaxDestruction = 1000000;
+            private CategoryGadgetDestruction() {
+            }
 
-            @RangeInt(min = 0, max = 100000)
-            @Name("Energy Cost")
-            @Comment(LANG_KEY_GADGETS_ENERGY_COMMENT)
-            @LangKey(LANG_KEY_GADGETS_ENERGY)
-            public int energyCostDestruction = 200;
+            public int energyMaxDestruction = config.get(CATEGORY_GADGET_DESTRUCTION, "Maximum Energy", 1000000, "The max energy of the Destruction Gadget", 0, Integer.MAX_VALUE).setLanguageKey(LANG_KEY_GADGET_DESTRUCTION + ".maxEnergy").getInt();
 
-            @RangeInt(min = 0, max = 2000)
-            @Name("Damage Cost")
-            @Comment(LANG_KEY_GADGETS_DAMAGE_COMMENT)
-            @LangKey(LANG_KEY_GADGETS_DAMAGE)
-            public int damageCostDestruction = 2;
+            public int energyCostDestruction = config.get(CATEGORY_GADGET_DESTRUCTION, "Energy Cost", 200, LANG_KEY_GADGETS_ENERGY_COMMENT, 0, 100000).setLanguageKey(LANG_KEY_GADGETS_ENERGY).getInt();
 
-            @RequiresWorldRestart
-            @RangeInt(min = 0, max = 100000)
-            @Name("Durability")
-            @Comment(LANG_KEY_GADGETS_DURABILITY_COMMENT)
-            @LangKey(LANG_KEY_GADGETS_DURABILITY)
-            public int durabilityDestruction = 10000;
+            public int damageCostDestruction = config.get(CATEGORY_GADGET_DESTRUCTION, "Damage Cost", 2, LANG_KEY_GADGETS_DAMAGE_COMMENT, 0, 2000).setLanguageKey(LANG_KEY_GADGETS_DAMAGE).getInt();
 
-            @RangeDouble(min = 0)
-            @Name("Non-Fuzzy Mode Multiplier")
-            @Comment("The cost in energy/durability will increase by this amount when not in fuzzy mode")
-            @LangKey(LANG_KEY_GADGET_DESTRUCTION + ".nonfuzzy.multiplier")
-            public double nonFuzzyMultiplier = 2;
+            public int durabilityDestruction = config.get(CATEGORY_GADGET_DESTRUCTION, "Durability", 10000, LANG_KEY_GADGETS_DURABILITY_COMMENT, 0, 100000).setLanguageKey(LANG_KEY_GADGETS_DURABILITY).setRequiresWorldRestart(true).getInt();
 
-            @Name("Non-Fuzzy Mode Enabled")
-            @Comment("If enabled, the Destruction Gadget can be taken out of fuzzy mode, allowing only instances of the block "
-                    + "clicked to be removed (at a higher cost)")
-            @LangKey(LANG_KEY_GADGET_DESTRUCTION + ".nonfuzzy.enabled")
-            public boolean nonFuzzyEnabled = false;
+            public double nonFuzzyMultiplier = config.get(CATEGORY_GADGET_DESTRUCTION, "Non-Fuzzy Mode Multiplier", 2, "The cost in energy/durability will increase by this amount when not in fuzzy mode", 0, Double.MAX_VALUE).setLanguageKey(LANG_KEY_GADGET_DESTRUCTION + ".nonfuzzy.multiplier").getDouble();
+
+            public boolean nonFuzzyEnabled = config.get(CATEGORY_GADGET_DESTRUCTION, "Non-Fuzzy Mode Enabled", false, "If enabled, the Destruction Gadget can be taken out of fuzzy mode, allowing only instances of the block clicked to be removed (at a higher cost)").setLanguageKey(LANG_KEY_GADGET_DESTRUCTION + ".nonfuzzy.enabled").getBoolean();
         }
 
         public static final class CategoryGadgetCopyPaste {
-            private CategoryGadgetCopyPaste() { }
+            private CategoryGadgetCopyPaste() {
+            }
 
-            @RangeInt(min = 0, max = 100000)
-            @Name("Energy Cost")
-            @Comment(LANG_KEY_GADGETS_ENERGY_COMMENT)
-            @LangKey(LANG_KEY_GADGETS_ENERGY)
-            public int energyCostCopyPaste = 50;
+            public int energyCostCopyPaste = config.get(CATEGORY_GADGET_COPY_PASTE, "Energy Cost", 50, LANG_KEY_GADGETS_ENERGY_COMMENT, 0, 100000).setLanguageKey(LANG_KEY_GADGETS_ENERGY).getInt();
 
-            @RangeInt(min = 0, max = 2000)
-            @Name("Damage Cost")
-            @Comment(LANG_KEY_GADGETS_DAMAGE_COMMENT)
-            @LangKey(LANG_KEY_GADGETS_DAMAGE)
-            public int damageCostCopyPaste = 1;
+            public int damageCostCopyPaste = config.get(CATEGORY_GADGET_COPY_PASTE, "Damage Cost", 1, LANG_KEY_GADGETS_DAMAGE_COMMENT, 0, 2000).setLanguageKey(LANG_KEY_GADGETS_DAMAGE).getInt();
 
-            @RequiresWorldRestart
-            @RangeInt(min = 0, max = 100000)
-            @Name("Durability")
-            @Comment(LANG_KEY_GADGETS_DURABILITY_COMMENT)
-            @LangKey(LANG_KEY_GADGETS_DURABILITY)
-            public int durabilityCopyPaste = 10000;
+            public int durabilityCopyPaste = config.get(CATEGORY_GADGET_COPY_PASTE, "Durability", 10000, LANG_KEY_GADGETS_DURABILITY_COMMENT, 0, 100000).setLanguageKey(LANG_KEY_GADGETS_DURABILITY).setRequiresWorldRestart(true).getInt();
         }
     }
 
@@ -271,23 +167,11 @@ public class Config {
         private CategoryPasteContainers() {
         }
 
-        @RangeInt(min = 1)
-        @Comment("The maximum capacity of a tier 1 (iron) Construction Paste Container")
-        @Name("T1 Container Capacity")
-        @LangKey(LANG_KEY_PASTE_CONTAINERS_CAPACITY + ".t1")
-        public int t1Capacity = 512;
+        public int t1Capacity = config.get(CATEGORY_PASTE_CONTAINERS, "T1 Container Capacity", 512, "The maximum capacity of a tier 1 (iron) Construction Paste Container", 1, Integer.MAX_VALUE).setLanguageKey(LANG_KEY_PASTE_CONTAINERS_CAPACITY + ".t1").getInt();
 
-        @RangeInt(min = 1)
-        @Comment("The maximum capacity of a tier 2 (gold) Construction Paste Container")
-        @Name("T2 Container Capacity")
-        @LangKey(LANG_KEY_PASTE_CONTAINERS_CAPACITY + ".t2")
-        public int t2Capacity = 2048;
+        public int t2Capacity = config.get(CATEGORY_PASTE_CONTAINERS, "T2 Container Capacity", 2048, "The maximum capacity of a tier 2 (gold) Construction Paste Container", 1, Integer.MAX_VALUE).setLanguageKey(LANG_KEY_PASTE_CONTAINERS_CAPACITY + ".t2").getInt();
 
-        @RangeInt(min = 1)
-        @Comment("The maximum capacity of a tier 3 (diamond) Construction Paste Container")
-        @Name("T3 Container Capacity")
-        @LangKey(LANG_KEY_PASTE_CONTAINERS_CAPACITY + ".t3")
-        public int t3Capacity = 8192;
+        public int t3Capacity = config.get(CATEGORY_PASTE_CONTAINERS, "T3 Container Capacity", 8192, "The maximum capacity of a tier 3 (diamond) Construction Paste Container", 1, Integer.MAX_VALUE).setLanguageKey(LANG_KEY_PASTE_CONTAINERS_CAPACITY + ".t3").getInt();
     }
 
 }
